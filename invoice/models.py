7b3ad3f2c40e616ef969e95214f373248ae892f8
Vinay <Vinay@example.com>
2023-09-30 19:29:52 +0530

    project set-up first time

---
+++ invoice/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Invoice(models.Model):
+    date = models.DateField(auto_now_add=True)
+    invoice_customer_name = models.CharField(max_length=255)
+
+
+class InvoiceDetail(models.Model):
+    invoice = models.ForeignKey(Invoice, on_delete=models.CASCADE)
+    description = models.CharField(max_length=255)
+    quantity = models.IntegerField()
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
